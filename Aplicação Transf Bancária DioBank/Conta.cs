951f0490a9fb0310e2fded5355968f192b8cc38c
Majulia <majulialcarvalho@gmail.com>
2021-10-24 21:17:32 -0300

    arrumando

--- Aplicação Transf Bancária DioBank/Conta.cs
+++ Aplicação Transf Bancária DioBank/Conta.cs
@@ -5,33 +5,23 @@ namespace DioBank
     public class Conta
     {
         private TipoConta TipoConta { get; set; }
-
         private double Saldo { get; set; }
-
         private double Credito { get; set; }
-
         private string Nome { get; set; }
-
         private string Cpf { get; set; }
 
 
-        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome, string cpf)
         {
-            //este é um método construtor: é chamado na hora de criar o objeto. ele terá os seguintes atributos:
-
             this.TipoConta = tipoConta;
             this.Saldo = saldo;
             this.Credito = credito;
             this.Nome = nome;
-            //this serve para especificar qual instância iremos alterar, é muito específico
-
-            //ele recebe os parâmetros, então na hora de criar um objeto devemos passar estes parâmetros
-            //e como é privado, não podemos mudar
+            this.Cpf = cpf;
         }
 
         public bool Sacar(double valorSaque)
         {
-            //Validação de saldo suficiente:
             if (this.Saldo - valorSaque < (this.Credito * (-1)))
             {
                 Console.WriteLine("Saldo Insuficiente");
@@ -52,10 +42,8 @@ namespace DioBank
 
         public void Transferir(double valorTransferencia, Conta contaDestino)
         {
-            //passamos o valor da transferência e para qual conta queremos transferir.
             if (this.Sacar(valorTransferencia))
             {
-            //reutilizando o método de sacar e o de depositar   
                 contaDestino.Depositar(valorTransferencia);
             }
         }
@@ -65,6 +53,7 @@ namespace DioBank
             string retorno = " ";
             retorno += " Tipo Conta " + this.TipoConta + " | ";
             retorno += "Nome " + this.Nome + " | ";
+            retorno += "CPF " + this.Cpf + " | ";
             retorno += "Saldo " + this.Saldo + " | ";
             retorno += "Crédito " + this.Credito + " | ";
             return retorno;
--- Aplicação Transf Bancária DioBank/Program.cs
+++ Aplicação Transf Bancária DioBank/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-
 namespace DioBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 100, 100, "Majulia");
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 100, 100, "Majulia", "09068787-96");
 
             Console.WriteLine(minhaConta.ToString());
             
