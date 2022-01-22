c399a56c467b11e7061b17f94a03a78d4fdb92a5
Majulia <majulialcarvalho@gmail.com>
2022-01-22 18:19:43 -0300

    exemplos de variáveis

---
+++ Trabalhando com Tipod de Referência e Valor/primeiro exemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+public class Program
+{
+    static int Adicionar20(int x)
+    {
+        return x + 20;
+    }
+    public static void Main()
+    {
+        int a = 2;
+        Adicionar20(a);
+
+        WriteLine($"Valor da variável a é {a}");
+    }
+
+}
