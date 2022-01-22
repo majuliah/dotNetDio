1fa80cc7002b98649fed37a34c2596ce294f8f01
Majulia <majulialcarvalho@gmail.com>
2022-01-22 19:14:42 -0300

    criando segundo exemplo

---
+++ Trabalhando com Tipod de Referência e Valor/segundo exemplo/Pessoa.cs
@@ -0,0 +1,7 @@
+public class Pessoa
+{
+    public int Idade { get; set; }
+    public string Nome { get; set; }
+    public string Documento { get; set; }
+
+}
---
+++ Trabalhando com Tipod de Referência e Valor/segundo exemplo/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+public class Program
+{
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+    public static void Main()
+    {
+       Pessoa p1 = new Pessoa();
+       p1.Nome = "Joao Silva";
+       p1.Idade = 30;
+       p1.Documento = "3546543540";
+
+    var p2 = p1;
+    //mesmo que
+    Pessoa p3 = p1;
+
+
+        TrocarNome(p1, "João SIlva Ano");
+        WriteLine(@$"
+        O novo nome de p1 é {p1.Nome}
+        O nome de p2 é {p2.Nome}
+        ");
+
+    }
+
+}
