a25892fa14c42d1dc8a33cee5713e6dc288d483e
Majulia <majulialcarvalho@gmail.com>
2022-01-22 19:53:02 -0300

    heap e stack

--- Trabalhando com Tipod de Referência e Valor/segundo exemplo/Program.cs
+++ Trabalhando com Tipod de Referência e Valor/segundo exemplo/Program.cs
@@ -1,11 +1,8 @@
 ﻿using static System.Console;
 public class Program
 {
-    static void TrocarNome(Pessoa p1, string nomeNovo)
-    {
-        p1.Nome = nomeNovo;
-    }
-    public static void Main()
+
+    static void Demo2()
     {
        Pessoa p1 = new Pessoa();
        p1.Nome = "Joao Silva";
@@ -24,6 +21,44 @@ public class Program
         O novo nome de p1 é {p1.Nome}
         O nome de p2 é {p2.Nome}
         ");
+
+    
+    }
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+
+    static void TrocarNome(StructPessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+
+    public static void Main()
+    {
+        StructPessoa p1 = new StructPessoa()
+        {
+            Documento = "44646563",
+            Idade = 30,
+            Nome = "Marajulha Carvalho"
+        };
+
+        StructPessoa p = default;
+           p.Documento = "6563";
+           p.Idade = 22;
+           p.Nome = "Marajulha Alves";
+
+        var p2 = p1;
+
+        TrocarNome(p1, "Joaquina Lima");
+        WriteLine(@$"
+        Nome do p1 {p1.Nome}
+        Nome do p2 {p2.Nome}
+        ");
+
+
+
+
 
     }
 
---
+++ Trabalhando com Tipod de Referência e Valor/segundo exemplo/StructPessoa.cs
@@ -0,0 +1,7 @@
+public struct StructPessoa
+{
+    public string Documento {get; set;}
+    public string Nome {get; set;}
+    public int Idade {get; set;}
+    
+}
