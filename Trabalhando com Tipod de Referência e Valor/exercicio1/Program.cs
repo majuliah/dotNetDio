763a184cb7a8933e0eb8133eeb9e5cf8795630c3
Majulia <majulialcarvalho@gmail.com>
2022-01-22 21:54:15 -0300

    precisa rever o cap

---
+++ Trabalhando com Tipod de Referência e Valor/exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+public class Program
+{
+
+    static int EncontrarNumero(int[] numeros, int numero)
+    {
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if(numeros[i] == numero)
+                return i;   
+        }
+        return -1;
+    }
+    public static void Main(){
+
+        int[] numeros = new int[] {0, 1, 2, 3, 4, 5};
+        WriteLine("Digite o número que gostaria de entrar");
+        int numero = int.Parse(ReadLine());
+
+        int indexEncontrado = EncontrarNumero(numeros, numero);
+        WriteLine($"O número encontrado está na posição {indexEncontrado}");
+    }
+    
+
+
+}
